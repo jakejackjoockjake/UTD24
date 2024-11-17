5ac9fac16e7a9bbecbcf237fe3813c4d567d6b7c
jakejackjoockjake <jacobglenreeves@gmail.com>
2024-11-17 09:04:43 -0600

    Removed secondary page code and restored functionality

--- HackUTD24/src/App.jsx
+++ HackUTD24/src/App.jsx
@@ -1,11 +1,8 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import { SignedIn, SignedOut, SignInButton, UserButton} from "@clerk/clerk-react";
 import './App.css';
-import logo from './assets/SWOT.jpg';
-
-
 export default function App() {
   return (
-    <><img src={logo} alt="Logo" className='logo'style="width: 300px; height: 300px;" /><header>
+    <><img src={"assets/SWOT.jpg"} alt="Logo" className='logo' /><header>
       <SignedOut>
         <SignInButton>Sign in Here!</SignInButton>
       </SignedOut>
